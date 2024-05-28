00532f75418842069f2bc5edd4fd53fd1335dc31
Titilope Ajayi <10183938-Titilegend@users.noreply.replit.com>
2024-05-28 08:56:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+name = input("Please enter your name: ")
+age =  input("Please enter your age: ")
+location = input("Please enter your location: ")
+print("Hello "+name+"! "+"You are "+age+"years old and you live in "+location+".")
